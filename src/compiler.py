a8d0c11a5af3ffbd47cd5a5b14d353c168597929
Bruce Li <isanoob1897@outlook.com>
2024-03-26 20:13:52 -0700

    add

---
+++ src/compiler.py
@@ -0,0 +1,32 @@
+# the code is not that difficult
+# However, debuging may be hard do to the fact
+# this is in assembly 
+# Note that this implementation is for x64. ARM users refer to the compilerARM.py file
+class CodeGenerator:
+    def __init__(self):
+        self.asm = []
+
+    def generate_code(self, ast):
+        self.asm.append(".intel_syntax noprefix")
+        self.asm.append(".text")
+        self.asm.append(".global _start")
+        self.asm.append("_start:")
+        self.traverse_ast(ast)
+        self.asm.append("    mov rdi, rax")  # Move the result to rdi (convention for exit syscall)
+        self.asm.append("    mov rax, 60")  # System call number for exit
+        self.asm.append("    syscall")      # Invoke exit syscall
+
+    def traverse_ast(self, node):
+        if node.type == 'expression':
+            self.traverse_ast(node.left)
+            self.asm.append("    push rax")  # Push the left operand onto the stack
+            self.traverse_ast(node.right)
+            self.asm.append("    pop rbx")   # Pop the left operand from the stack to rbx
+        elif node.type == 'integer':
+            self.asm.append(f"    mov rax, {node.value}")  # Move the integer value to rax
+        elif node.type == 'operator':
+            self.asm.append("    pop rdi")   # Pop the right operand from the stack to rdi
+            self.asm.append("    add rax, rdi" if node.value == '+' else
+                             "    sub rax, rdi" if node.value == '-' else
+                             "    imul rax, rdi" if node.value == '*' else
+                             "    cqo\n    idiv rdi") 
---
+++ src/compilerARM.py
@@ -0,0 +1,30 @@
+# The Arm implementation
+# x64 users look at compiler.py
+class CodeGenerator:
+    def __init__(self):
+        self.asm = []
+
+    def generate_code(self, ast):
+        self.asm.append(".text")
+        self.asm.append(".global _start")
+        self.asm.append("_start:")
+        self.asm.append("    mov x29, sp")  # Set up the frame pointer
+        self.traverse_ast(ast)
+        self.asm.append("    mov x0, x1")  # Move the result to x0 (convention for exit syscall)
+        self.asm.append("    mov x8, 93")  # System call number for exit
+        self.asm.append("    svc #0")      # Invoke exit syscall
+
+    def traverse_ast(self, node):
+        if node.type == 'expression':
+            self.traverse_ast(node.left)
+            self.asm.append("    str x0, [sp, #-8]!")  # Push the left operand onto the stack
+            self.traverse_ast(node.right)
+            self.asm.append("    ldr x1, [sp], #8")    # Pop the left operand from the stack to x1
+        elif node.type == 'integer':
+            self.asm.append(f"    mov x0, #{node.value}")  # Move the integer value to x0
+        elif node.type == 'operator':
+            self.asm.append("    ldr x1, [sp]")   # Load the right operand from the stack to x1
+            self.asm.append("    add x0, x1, x0" if node.value == '+' else
+                             "    sub x0, x1, x0" if node.value == '-' else
+                             "    mul x0, x1, x0" if node.value == '*' else
+                             "    sdiv x0, x1, x0")  # Divide x1 by x0 (signed division)
